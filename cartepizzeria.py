d2814f3b272f618f8e98fc646ff3413b4e51d6c4
ZAID <leticia.zaid.lz@gmail.com>
2026-03-30 13:59:45 +0200

    Ajout des classes Pizza, CartePizzeria et Exception

---
+++ cartepizzeria.py
@@ -0,0 +1,20 @@
+from cartepizzeriaexception import CartePizzeriaException
+class CartePizzeria:
+    def __init__(self):
+        self.pizzas = []
+
+    def is_empty(self):
+        return len(self.pizzas) == 0
+
+    def nb_pizzas(self):
+        return len(self.pizzas)
+
+    def add_pizza(self, pizza):
+        self.pizzas.append(pizza)
+
+    def remove_pizza(self, name):
+        for pizza in self.pizzas:
+            if pizza.name == name:
+                self.pizzas.remove(pizza)
+                return
+        raise CartePizzeriaException(f"La pizza '{name}' n'existe pas")
